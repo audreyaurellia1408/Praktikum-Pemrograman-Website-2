7f93ac41c4f6c8ab390404cc14033b1d8e5a6003
audreyaurellia1408 <audreyaurellia1408@gmail.com>
2025-10-21 17:23:03 +0700

    commit

--- pertemuan4_koneksi/proses_login.php
+++ pertemuan4_koneksi/proses_login.php
@@ -5,21 +5,19 @@ include 'koneksi.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// Cek apakah username ada di database
 $query = "SELECT * FROM users WHERE username='$username'";
 $result = mysqli_query($koneksi, $query);
 
 if (mysqli_num_rows($result) > 0) {
     $data = mysqli_fetch_assoc($result);
 
-    // Verifikasi password
+
     if (password_verify($password, $data['password'])) {
-        // Simpan data ke session
+
         $_SESSION['username'] = $data['username'];
         $_SESSION['role'] = $data['role'];
         $_SESSION['login'] = true;
 
-        // Arahkan berdasarkan role
         if ($data['role'] == 'admin') {
             header("Location: dashboard_admin.php");
         } else {
--- pertemuan4_koneksi/proses_tambah_user.php
+++ pertemuan4_koneksi/proses_tambah_user.php
@@ -1,15 +1,12 @@
 <?php
 include 'koneksi.php';
 
-// Ambil data dari form
 $username = $_POST['username'];
 $password = $_POST['password'];
 $role     = $_POST['role'];
 
-// Enkripsi password
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-// Query simpan ke database (tanpa nama_lengkap dan status)
 $query = "INSERT INTO users (username, password, role, created_at)
           VALUES ('$username', '$hashed_password', '$role', NOW())";
 
