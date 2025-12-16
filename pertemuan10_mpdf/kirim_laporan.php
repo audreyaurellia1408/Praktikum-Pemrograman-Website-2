fec0889a209d2788d84e7f761cd1ff1b6a32eb01
audreyaurellia1408 <audreyaurellia1408@gmail.com>
2025-12-16 16:27:46 +0700

    perbaikan

--- pertemuan10_mpdf/kirim_laporan.php
+++ pertemuan10_mpdf/kirim_laporan.php
@@ -55,8 +55,8 @@ if(isset($_POST['email'])) {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'audreyaurellia1408@gmail.com';
-        $mail->Password   = 'ullg leoq abnp lrrz';    
+        $mail->Username   = '';
+        $mail->Password   = '';    
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
